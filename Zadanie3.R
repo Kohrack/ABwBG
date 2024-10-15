e2b76b7531585417db158fba166aa28568c4b884
Kohrack <h.omer@wp.pl>
2024-10-15 12:42:38 +0200

    Add files via upload
    Zadanie 3

---
+++ Zadanie3.R
@@ -0,0 +1,33 @@
+---
+title: "Zadanie3"
+output: html_document
+date: "2024-10-15"
+---
+
+# Raport obejmuje informacje dotyczące pakietu ioanalisis
+
+Pakiety które się w nim znajdują to ggplot2 , plot3D, lpSolve. \# w celu wczytaniago użyłem komendy
+
+```{r}
+> install.packages("ioanalysis")
+```
+
+i w celu zapoznania się z poszczegulnymi pakietami użyłem komendy
+
+```{r}
+> library(ioanalysis)
+```
+
+i do zapoznania się z poszczególnymi jego elementami i komedami użyłem komendy
+```{r}
+> ?ioanalysis
+```
+Pakiet wydaje się służyć jako narzędzie do tworzenia list input , output w celu oszczędzania czasu w pracy nad takimi danymi. Dotyczny on i odnosi się di kalkulacji Goshian inverse i Leontief inverse . Wydają się to być terminy matematyczne
+## Przykład kodu 
+```{r}
+>as.inputoutput(E)
+
+```
+*jest kodem odpowiedzialnymza podstawe pakiety w którewstawiane są przykładowo dane E, czyli eksporty*
+# Konkluzje
+Jest to pakiet przeznaczony do analiz których nie rozumiem w **tym momencie**
